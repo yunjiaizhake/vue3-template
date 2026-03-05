cd5cf14088ff0718660559989bc157118e166e26
s_lihongbo <s_lihongbo@wps.cn>
2026-03-05 09:46:31 +0800

    fix: 将前端配置文件数据从对象形式改为函数形式，以此能更好的设置一些参数

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import { resolve } from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
-      dts: 'src/types/auto-imports.d.ts', // 生成 TS 类型声明
-      eslintrc: {
-        enabled: true, // 生成 eslint 配置
-        filepath: './.eslintrc-auto-import.json',
-      },
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        additionalData: `@import "src/styles/var.less"; @import "src/styles/mixin.less";`,
+export default defineConfig(({ mode }) => {
+  console.log('当前运行模式 mode:', mode);
+
+  const env = loadEnv(mode, process.cwd());
+  console.log('加载到的环境变量 env:', env);
+
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+        dts: 'src/types/auto-imports.d.ts',
+        eslintrc: { enabled: true, filepath: './.eslintrc-auto-import.json' },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `@import "src/styles/var.less"; @import "src/styles/mixin.less";`,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+    resolve: { alias: { '@': resolve(__dirname, './src') } },
+  };
 });
